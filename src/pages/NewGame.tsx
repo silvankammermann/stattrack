785f63d88434f73cbe0836c9f76134339dd41a9b
Silvan Kammermann <silvan.kammermann@students.fhnw.ch>
2025-08-04 23:13:35 +0200

    update imports

--- src/pages/NewGame.tsx
+++ src/pages/NewGame.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { Game, Player } from "../model/types";
+import { Game } from "../model/types";
 import Toggle from "../components/Toggle";
 import { nextId, saveGame } from "../storage/local";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function NewGame() {
   const [gameForm, setGameForm] = useState<Game>({
