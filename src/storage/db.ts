89eee7918f79cce4dabd4ffef51c425b740dd418
Silvan Kammermann <silvan.kammermann@students.fhnw.ch>
2025-08-07 22:27:08 +0200

    Add real game data

--- src/storage/db.ts
+++ src/storage/db.ts
@@ -6,23 +6,49 @@ const data: Game[] = [{
     name: "BC Gegner",
     score: 0
   },
-  date: "3. Mai 2025",
-  homeGame: false,
+  date: "2. Mai 2025",
+  homeGame: true,
   score: 20,
   players: [{
-    nr: 14,
-    name: "Silvan",
+    nr: 4,
+    name: "Dave",
     stats: {
       twoPt: {
+        made: 3,
+        missed: 7
+      },
+      threePt: {
         made: 2,
-        missed: 1
+        missed: 2
+      },
+      ft: {
+        made: 7,
+        missed: 3
+      },
+      ast: 7,
+      reb: {
+        off: 0,
+        def: 12
+      },
+      stl: 4,
+      blk: 3,
+      to: 2
+    }
+  },
+  {
+    nr: 5,
+    name: "Jasper",
+    stats: {
+      twoPt: {
+        made: 0,
+        missed: 4
       },
       threePt: {
         made: 0,
         missed: 0
       },
       ft: {
-        made: 0,
+        made: 2,
         missed: 0
       },
       ast: 0,
@@ -36,16 +62,94 @@ const data: Game[] = [{
     }
   },
   {
-    nr: 14,
-    name: "Silvan",
+    nr: 6,
+    name: "Edi",
     stats: {
       twoPt: {
-        made: 2,
-        missed: 1
+        made: 4,
+        missed: 4
       },
       threePt: {
         made: 0,
+        missed: 1
+      },
+      ft: {
+        made: 1,
+        missed: 1
+      },
+      ast: 1,
+      reb: {
+        off: 0,
+        def: 6
+      },
+      stl: 0,
+      blk: 0,
+      to: 3
+    }
+  },
+  {
+    nr: 8,
+    name: "Jan",
+    stats: {
+      twoPt: {
+        made: 0,
         missed: 0
+      },
+      threePt: {
+        made: 0,
+        missed: 4
+      },
+      ft: {
+        made: 1,
+        missed: 1
+      },
+      ast: 0,
+      reb: {
+        off: 0,
+        def: 1
+      },
+      stl: 0,
+      blk: 0,
+      to: 2
+    }
+  },
+  {
+    nr: 9,
+    name: "Nevio",
+    stats: {
+      twoPt: {
+        made: 5,
+        missed: 10
+      },
+      threePt: {
+        made: 0,
+        missed: 1
+      },
+      ft: {
+        made: 1,
+        missed: 1
+      },
+      ast: 3,
+      reb: {
+        off: 2,
+        def: 8
+      },
+      stl: 1,
+      blk: 0,
+      to: 3
+    }
+  },
+  {
+    nr: 10,
+    name: "Simon",
+    stats: {
+      twoPt: {
+        made: 5,
+        missed: 6
+      },
+      threePt: {
+        made: 3,
+        missed: 3
       },
       ft: {
         made: 0,
@@ -54,11 +158,63 @@ const data: Game[] = [{
       ast: 0,
       reb: {
         off: 0,
-        def: 0
+        def: 5
+      },
+      stl: 1,
+      blk: 1,
+      to: 3
+    }
+  },
+  {
+    nr: 11,
+    name: "Mischa",
+    stats: {
+      twoPt: {
+        made: 4,
+        missed: 5
+      },
+      threePt: {
+        made: 0,
+        missed: 1
+      },
+      ft: {
+        made: 2,
+        missed: 0
+      },
+      ast: 2,
+      reb: {
+        off: 1,
+        def: 1
+      },
+      stl: 1,
+      blk: 0,
+      to: 2
+    }
+  },
+  {
+    nr: 12,
+    name: "Stifu",
+    stats: {
+      twoPt: {
+        made: 1,
+        missed: 1
+      },
+      threePt: {
+        made: 0,
+        missed: 0
+      },
+      ft: {
+        made: 2,
+        missed: 0
+      },
+      ast: 0,
+      reb: {
+        off: 1,
+        def: 1
       },
       stl: 0,
       blk: 0,
-      to: 0
+      to: 2
     }
   }]
 }];
